6fa9c3cbce718c23ed5ad73902419dc03b093c3c
solanadeveloper10 <solanadeveloper10@gmail.com>
2025-01-31 18:09:15 +0200

    CA

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,19 @@ function App() {
     <div className='color-bg' />
     <div className='wrapper'>
       <header className='header'>
-        <a href="#" target='_blank' className='social-link'>
+        <a href="#" target='_blank' className='https://x.com/CancelMrPunk'>
           X
         </a>
-        <a href="#" target='_blank' className='social-link'>
+        <a href="#" target='_blank' className='https://t.me/CancelMrPunk'>
           Telegram
         </a>
-        <a href="#" target='_blank' className='social-link'>
+        <a href="#" target='_blank' className='https://www.dextools.io/app/en/solana/pair-explorer/5STy8JRiTeSvdSNjjqQBCUmDNsL8Y5ZZ4XxcyP1ppgjY?t=1738339718383'>
           Dextools
         </a>
-        <a href="#" target='_blank' className='social-link'>
+        <a href="#" target='_blank' className='https://dexscreener.com/solana/igRHkYv54WYHgN4Y3HxJrWrJwV9BTbwntFwwStMq6od'>
           Dexscreener
         </a>
-        <a href="#" target='_blank' className='social-link'>
+        <a href="#" target='_blank' className='http://raydium.io/swap/?inputMint=sol&outputMint=igRHkYv54WYHgN4Y3HxJrWrJwV9BTbwntFwwStMq6od'>
           Raydium
         </a>
       </header>
